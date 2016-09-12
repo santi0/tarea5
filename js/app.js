72e82da5e5d46d7d9ce5e0c1982ed013fb5f23c8
santiago <santiagoesquivel86@gmail.com>
2016-09-12 13:34:48 -0400

    se actualizo tarea5

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+(function () {
+
+
+	console.log('Hola Mundo');
+
+   }());
+
+
+var num= 123;
+ 
+var texto ='Hola Mundo JS';
+
+var lista [];
+
+var persona = {};
+
+
+lista.push(15);
+
+lista.push(75);
+
+console.log(lista);
+
+
+var miobjeto ={};
+
+miobjeto.descripcion = 'Notebok';
+miobjeto.cantidad = '2';
+
+console.log (miobjeto);
+
+
+
+
